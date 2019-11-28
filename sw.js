8a40af729bdbcc476bb92a637e801545e05325df
laiczhang <zy7298@icloud.com>
2019-11-28 10:47:22 +0800

    Site updated: 2019-11-28 10:47:17

--- sw.js
+++ sw.js
@@ -24,15 +24,15 @@ var precacheUrls = [];
 
   precacheUrls.push('/');
 
-  precacheUrls.push('/2019-11-06/关于攻坚性项目的一些建议/');
+  precacheUrls.push('/2019-10-25/喜马拉雅-WPS-樊登会员-稻壳会员免费领取/');
 
   precacheUrls.push('/2019-07-20/提问的智慧/');
 
-  precacheUrls.push('/2019-09-27/CSDN下载的部分资源-2/');
+  precacheUrls.push('/2019-10-16/CSDN下载的部分资源-4/');
 
   precacheUrls.push('/2019-10-06/CSDN下载的部分资源-3/');
 
-  precacheUrls.push('/2019-11-08/科学上网/');
+  precacheUrls.push('/2019-09-27/CSDN下载的部分资源-2/');
 
 toolbox.precache(precacheUrls);
 toolbox.options = {"networkTimeoutSeconds":5};
