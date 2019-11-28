49e652c2ff87eadb20f5dd690dbd5b74f21d1e5d
laiczhang <zy7298@icloud.com>
2019-11-28 11:21:11 +0800

    Site updated: 2019-11-28 11:21:08

--- sw.js
+++ sw.js
@@ -24,15 +24,15 @@ var precacheUrls = [];
 
   precacheUrls.push('/');
 
-  precacheUrls.push('/2019-10-25/喜马拉雅-WPS-樊登会员-稻壳会员免费领取/');
+  precacheUrls.push('/2019-07-28/史上最全的开发和设计资源大全/');
 
   precacheUrls.push('/2019-07-20/提问的智慧/');
 
   precacheUrls.push('/2019-10-16/CSDN下载的部分资源-4/');
 
-  precacheUrls.push('/2019-10-06/CSDN下载的部分资源-3/');
-
   precacheUrls.push('/2019-09-27/CSDN下载的部分资源-2/');
+
+  precacheUrls.push('/2019-10-06/CSDN下载的部分资源-3/');
 
 toolbox.precache(precacheUrls);
 toolbox.options = {"networkTimeoutSeconds":5};
