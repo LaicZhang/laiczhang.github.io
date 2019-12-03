27f821348d72f17b0cae01574b71ce3930e4158b
laiczhang <zy7298@icloud.com>
2019-12-04 00:31:43 +0800

    这个勤奋的家伙又更新了: 2019-12-04 00:31:31

--- sw.js
+++ sw.js
@@ -24,7 +24,7 @@ var precacheUrls = [];
 
   precacheUrls.push('/');
 
-  precacheUrls.push('/2019-11-05/如何了解网络信息安全最新的一些动态/');
+  precacheUrls.push('/2019-10-25/喜马拉雅-WPS-樊登会员-稻壳会员免费领取/');
 
   precacheUrls.push('/2019-07-20/提问的智慧/');
 
