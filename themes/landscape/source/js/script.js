1e80494c60c12f963db03646d0bd799b85086542
laiczhang <i@laiczhang.com>
2024-12-25 02:24:55 +0800

    chore: update script

--- themes/landscape/source/js/script.js
+++ themes/landscape/source/js/script.js
@@ -1,158 +1,152 @@
-(function($){
-  // Search
-  let $searchWrap = $('#search-form-wrap'),
-    isSearchAnim = false,
-    searchAnimDuration = 200;
+(function($) {
+  // --- Search Functionality ---
+  const $searchWrap = $('#search-form-wrap');
+  let isSearchAnim = false;
+  const searchAnimDuration = 200;
 
-  let startSearchAnim = function(){
+  const toggleSearchAnim = (state, callback) => {
+    if (isSearchAnim) return; // Avoid double execution during animation
+
     isSearchAnim = true;
+
+    $searchWrap.toggleClass('on', state === 'start'); // Using toggleClass for cleaner code
+
+    if (state === 'stop') {
+      setTimeout(() => {
+        isSearchAnim = false;
+        callback && callback();
+      }, searchAnimDuration);
+    } else if(state === 'start' && callback){
+        callback();
+    }
   };
 
-  let stopSearchAnim = function(callback){
-    setTimeout(function(){
-      isSearchAnim = false;
-      callback && callback();
-    }, searchAnimDuration);
-  };
-
-  $('#nav-search-btn').on('click', function(){
-    if (isSearchAnim) return;
-
-    startSearchAnim();
-    $searchWrap.addClass('on');
-    stopSearchAnim(function(){
-      $('.search-form-input').focus();
-    });
+  $('#nav-search-btn').on('click', () => {
+    toggleSearchAnim('start', () => $('.search-form-input').focus());
   });
 
-  $('.search-form-input').on('blur', function(){
-    startSearchAnim();
-    $searchWrap.removeClass('on');
-    stopSearchAnim();
+  $('.search-form-input').on('blur', () => {
+    toggleSearchAnim('stop');
   });
 
-  let pathName = window.location.pathname;
-  if (pathName === '/' || pathName.startsWith('/page/')){
-    $('.article-share-link').remove();
-  }else{
-  // Share
-  $('body').on('click', function(){
-    $('.article-share-box.on').removeClass('on');
-  }).on('click', '.article-share-link', function(e){
-    e.stopPropagation();
+  // --- Share Functionality ---
+  const pathName = window.location.pathname;
+  if (pathName !== '/' && !pathName.startsWith('/page/')) { // Simplified condition
 
-    var $this = $(this),
-      url = $this.attr('data-url'),
-      encodedUrl = encodeURIComponent(url),
-      id = 'article-share-box-' + $this.attr('data-id'),
-      title = $this.attr('data-title'),
-      offset = $this.offset();
+    const $body = $('body');
 
-    if ($('#' + id).length){
-      var box = $('#' + id);
-
-      if (box.hasClass('on')){
-        box.removeClass('on');
-        return;
+    $body.on('click', (e) => {
+      if (!$(e.target).closest('.article-share-box').length) {
+        $('.article-share-box.on').removeClass('on').hide(); // Hide the open share box
       }
-    } else {
-      var html = [
-        '<div id="' + id + '" class="article-share-box">',
-          '<!--<input class="article-share-input" value="' + url + '">-->',
-          '<div class="article-share-links">',
-            '<a href="https://x.com/intent/tweet?text=' + encodeURIComponent(title) + '&url=' + encodedUrl + '" class="article-share-twitter" target="_blank" title="Twitter"></a>',
-            '<a href="https://www.facebook.com/sharer.php?u=' + encodedUrl + '" class="article-share-facebook" target="_blank" title="Facebook"></a>',
-            '<a href="http://pinterest.com/pin/create/button/?url=' + encodedUrl + '" class="article-share-pinterest" target="_blank" title="Pinterest"></a>',
-            '<a href="https://www.linkedin.com/shareArticle?mini=true&url=' + encodedUrl + '" class="article-share-linkedin" target="_blank" title="LinkedIn"></a>',
-          '</div>',
-        '</div>'
-      ].join('');
+    }).on('click', '.article-share-link', function(e) {
+        e.stopPropagation();
 
-      var box = $(html);
+      const $this = $(this);
+      const url = $this.data('url'); // Use .data() to get data attributes
+      const encodedUrl = encodeURIComponent(url);
+      const id = `article-share-box-${$this.data('id')}`;
+      const title = $this.data('title');
+      const offset = $this.offset();
 
-      $('body').append(box);
+      let $box = $('#' + id);
+
+      if ($box.length) {
+          $box.toggleClass('on').toggle();
+          return;
+      }
+
+      const html = `
+          <div id="${id}" class="article-share-box">
+            <div class="article-share-links">
+              <a href="https://x.com/intent/tweet?text=${encodeURIComponent(title)}&url=${encodedUrl}" class="article-share-twitter" target="_blank" title="Twitter"></a>
+              <a href="https://www.facebook.com/sharer.php?u=${encodedUrl}" class="article-share-facebook" target="_blank" title="Facebook"></a>
+              <a href="http://pinterest.com/pin/create/button/?url=${encodedUrl}" class="article-share-pinterest" target="_blank" title="Pinterest"></a>
+              <a href="https://www.linkedin.com/shareArticle?mini=true&url=${encodedUrl}" class="article-share-linkedin" target="_blank" title="LinkedIn"></a>
+            </div>
+          </div>
+        `;
+
+        $box = $(html).appendTo($body); // Use appendTo for performance
+        $('.article-share-box.on').removeClass('on').hide();
+      $box.css({
+          top: offset.top + 25,
+          left: offset.left
+      }).addClass('on').show();
+    }).on('click', '.article-share-box', (e) => e.stopPropagation()) //  Prevent box clicks from closing the box
+    .on('click', '.article-share-box-link', function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        try {
+            window.open(this.href, 'article-share-box-window-' + Date.now(), 'width=500,height=450');
+          } catch (error) {
+            console.error("Failed to open share window:", error);
+          }
+    });
+  } else {
+    $('.article-share-link').remove();
+  }
+
+  // --- Caption Functionality ---
+  $('.article-entry').each(function(i) {
+    $(this).find('img').each(function() {
+      if ($(this).parent().hasClass('fancybox') || $(this).parent().is('a')) return;
+      const alt = this.alt;
+      if (alt) {
+        $(this).after(`<span class="caption">${alt}</span>`)
+          .wrap(`<a href="${this.src}" data-fancybox="gallery" data-caption="${alt}"></a>`);
+      }
+    });
+
+    $(this).find('.fancybox').attr('rel', `article${i}`);
+  });
+
+    if ($.fancybox) {
+        $('.fancybox').fancybox();
     }
 
-    $('.article-share-box.on').hide();
+  // --- Mobile Navigation Functionality ---
+    const $container = $('#container');
+    let isMobileNavAnim = false;
+    const mobileNavAnimDuration = 200;
 
-    box.css({
-      top: offset.top + 25,
-      left: offset.left
-    }).addClass('on');
-  }).on('click', '.article-share-box', function(e){
-    e.stopPropagation();
-  })
-  // .on('click', '.article-share-box-input', function(){
-  //   $(this).select();
-  // })
-  .on('click', '.article-share-box-link', function(e){
-    e.preventDefault();
-    e.stopPropagation();
+    const toggleMobileNavAnim = (state) => {
+      if (isMobileNavAnim) return;
 
-    window.open(this.href, 'article-share-box-window-' + Date.now(), 'width=500,height=450');
-  });
-}
+        isMobileNavAnim = true;
 
-  // Caption
-  $('.article-entry').each(function(i){
-    $(this).find('img').each(function(){
-      if ($(this).parent().hasClass('fancybox') || $(this).parent().is('a')) return;
+        $container.toggleClass('mobile-nav-on', state === 'start');
 
-      var alt = this.alt;
+      if (state === 'stop') {
+          setTimeout(() => {
+            isMobileNavAnim = false;
+          }, mobileNavAnimDuration);
+      }
+    };
 
-      if (alt) $(this).after('<span class="caption">' + alt + '</span>');
+    $('#main-nav-toggle').on('click', () => {
+        toggleMobileNavAnim('start');
+      });
 
-      $(this).wrap('<a href="' + this.src + '" data-fancybox=\"gallery\" data-caption="' + alt + '"></a>')
-    });
+    $('#wrap').on('click', () => {
+        if(isMobileNavAnim || !$container.hasClass('mobile-nav-on')) return;
 
-    $(this).find('.fancybox').each(function(){
-      $(this).attr('rel', 'article' + i);
-    });
-  });
+        toggleMobileNavAnim('stop');
+        $container.removeClass('mobile-nav-on');
+      });
 
-  if ($.fancybox){
-    $('.fancybox').fancybox();
-  }
-
-  // Mobile nav
-  var $container = $('#container'),
-    isMobileNavAnim = false,
-    mobileNavAnimDuration = 200;
-
-  var startMobileNavAnim = function(){
-    isMobileNavAnim = true;
-  };
-
-  var stopMobileNavAnim = function(){
-    setTimeout(function(){
-      isMobileNavAnim = false;
-    }, mobileNavAnimDuration);
-  }
-
-  $('#main-nav-toggle').on('click', function(){
-    if (isMobileNavAnim) return;
-
-    startMobileNavAnim();
-    $container.toggleClass('mobile-nav-on');
-    stopMobileNavAnim();
-  });
-
-  $('#wrap').on('click', function(){
-    if (isMobileNavAnim || !$container.hasClass('mobile-nav-on')) return;
-
-    $container.removeClass('mobile-nav-on');
-  });
 })(jQuery);
 
+// --- Lazy Background Image Loading ---
 document.addEventListener("DOMContentLoaded", function() {
-  let lazyBackgrounds = [].slice.call(document.querySelectorAll("#banner"));
+    const lazyBackgrounds = [...document.querySelectorAll("#banner")];
 
   if ("IntersectionObserver" in window) {
-    let lazyBackgroundObserver = new IntersectionObserver(function(entries, observer) {
+    const lazyBackgroundObserver = new IntersectionObserver(function(entries, observer) {
       entries.forEach(function(entry) {
         if (entry.isIntersecting) {
           entry.target.classList.add("visible");
-          lazyBackgroundObserver.unobserve(entry.target);
+          observer.unobserve(entry.target);
         }
       });
     });
